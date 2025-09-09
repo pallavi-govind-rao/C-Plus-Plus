50e4d990536c337771c3f256e7b32104f6f7b0bc
Pallavi Govind Rao <govindraopallavi@gmail.com>
2025-09-09 11:38:05 +0530

    Update variables.cpp

--- Basics/variables.cpp
+++ Basics/variables.cpp
@@ -1,1 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello World"<<endl;
+    cout<<"hello coder"<<endl;
+    int x;
+    x=7;
+    cout<<x<<endl;
+    x=19;
+    cout<<x<<endl;
+    x = x+4;
+    cout<<x;
+}
 

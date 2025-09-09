5647d48c278dd2f162c409bff482ab276589484d
Pallavi Govind Rao <govindraopallavi@gmail.com>
2025-09-09 11:46:57 +0530

    Update factorial.cpp

--- Loop/factorial.cpp
+++ Loop/factorial.cpp
@@ -1,1 +1,12 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number :- ";
+    cin>>n;
+    int fact=1;
+    for(int i=2;i<=n;i++){
+        fact*=i;
+    }
+    cout<<"The factorial of "<<n<<" is "<<fact;
+}

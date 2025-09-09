10b3d0f793d4293ec555e365f7c2252fd718d179
Pallavi Govind Rao <govindraopallavi@gmail.com>
2025-09-09 11:41:40 +0530

    Update profitOrLoss.cpp

--- Conditional Statement/profitOrLoss.cpp
+++ Conditional Statement/profitOrLoss.cpp
@@ -1,1 +1,19 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+    int cp;
+    cout<<"Enter the cost price : ";
+    cin>>cp;
+    int sp;
+    cout<<"Enter the selling price : ";
+    cin>>sp;
+    if(cp==sp){
+        cout<<"Neither profit nor loss";
+    }
+    else if(cp>sp){
+        cout<<"loss = "<<cp-sp;
+    }
+    else{
+        cout<<"Profit = "<<sp-cp;
+    }
+}
